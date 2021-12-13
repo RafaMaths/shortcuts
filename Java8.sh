51bafb6917492940b34366c9e31f80f50e1f230f
Rafael Mathias <76234176+RafaMaths@users.noreply.github.com>
2021-12-13 10:00:48 -0300

    Update Java8.sh
    Alternativa para escolha de versão Java

--- Java8.sh
+++ Java8.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sudo apt install openjdk-8-jdk
+
+### Para escolher versões diferentes do java dentro do OS, basta descomentar e rodar o comando abaixo:
+# sudo update-alternatives --config java
